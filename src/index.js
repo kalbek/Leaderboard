a9490fc018b12d0d5b83d3b6d6541eec77bb06d6
kalbek <access2kaleb@gmail.com>
2023-04-12 22:42:22 +0300

    add event listeners for leaderboard actions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 // import _ from 'lodash';
 import './style.css';
+import Leaderboard from './modules/Leaderboard.js';
 
 function component() {
   const element = document.createElement('div');
+
+  const addScore = document.getElementById('submit');
+  addScore.addEventListener('click', (e) => {
+    e.preventDefault();
+    Leaderboard.addScore();
+  });
+
+  const refresh = document.querySelector('.refresh');
+  refresh.addEventListener('click', () => {
+    Leaderboard.updateUI();
+  });
   return element;
 }
+
 document.body.appendChild(component());

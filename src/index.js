ed1086f7450b574eb291b66b24fe7b3591b5f646
kalbek <access2kaleb@gmail.com>
2023-04-12 23:38:08 +0300

    implement add scores from api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-// import _ from 'lodash';
 import './style.css';
+import Leaderboard from './modules/Leaderboard.js';
 
 function component() {
   const element = document.createElement('div');
+
+  const addScore = document.getElementById('submit');
+  addScore.addEventListener('click', (e) => {
+    e.preventDefault();
+    Leaderboard.addScore();
+  });
+
   return element;
 }
+
 document.body.appendChild(component());
---
+++ src/modules/Leaderboard.js
@@ -0,0 +1,30 @@
+class Leaderboard {
+    // define api end-point
+    static apiEndPoint =
+      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/5KvA5Psull4GbZZmbL4r/scores/';
+
+    static addScore = async () => {
+      // grab the name and score from input
+      const name = document.getElementById('name');
+      const score = document.getElementById('score');
+
+      const action = {
+        method: 'POST',
+        body: JSON.stringify({
+          user: name.value,
+          score: score.value,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      };
+      // post a user and score on api
+      if (name.value !== null && score.value !== null) {
+        await fetch(this.apiEndPoint, action).then((response) => response.json());
+      }
+      // clear name and score fields
+      name.value = '';
+      score.value = '';
+    };
+}
+module.exports = Leaderboard;

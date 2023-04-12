9421b09f7f3acb858ad26489c3b661ff3716164f
kalbek <access2kaleb@gmail.com>
2023-04-12 16:19:41 +0300

    fix linters, clean code and ready for submission

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 // import _ from 'lodash';
 import './style.css';
- 
+
 function component() {
   const element = document.createElement('div');
- 
-
   return element;
 }
 document.body.appendChild(component());
